2cbcfbb3678346600cc2cf5e36e079dc030b39a1
wirde <github@wirde.com>
2010-01-08 23:14:21 +0100

    added functions map & filter

--- src/com/wirde/myscheme/primitives.scm
+++ src/com/wirde/myscheme/primitives.scm
@@ -9,11 +9,22 @@
 		#t
 		#f)))
 		
-(define reduce (lambda (f l initial) 
+(define (reduce f l initial) 
 	(if (nil? l)
 		initial
-		(f (car l) (reduce f (cdr l) initial)))))
+		(f (car l) (reduce f (cdr l) initial))))
 
+(define (map f l)
+	(if (nil? l)
+		nil
+		(cons (f (car l)) (map f (cdr l)))))
+		
+(define (filter f l)
+	(if (nil? l)
+		nil
+		(if (f (car l))
+			(cons (car l) (filter f (cdr l)))
+			(filter f (cdr l))))) 
 
 ;;(let ((x 1) (y 2))
 ;;forms
@@ -23,7 +34,7 @@
 ;;	forms
 ;;) 1 2)
 
-(define let (lambda (bindings . forms)
-	((lambda (x y)
-		(car forms)
-	) 1 2)))
+;;(define let (lambda (bindings . forms)
+;;	((lambda (x y)
+;;		(car forms)
+;;	) 1 2)))
